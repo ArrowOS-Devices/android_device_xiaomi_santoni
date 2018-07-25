fe3f335967baee07bf52bebd0879c66b2785c8e2
Sonal Singh <sonal.singh.19993@gmail.com>
2018-07-25 14:04:05 +0000

    ArrowOS Bringup

--- arrow.mk
+++ arrow.mk
@@ -16,12 +16,12 @@
 
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 
-# Inherit some common AEX stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720
 
-PRODUCT_NAME := aosp_santoni
+PRODUCT_NAME := arrow_santoni
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
